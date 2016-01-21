27b29ebd6ce7be44673bff6082f08530db3c47bc
Thiago Heitling <thiago.heitling@gmail.com>
2016-01-21 11:16:12 -0800

    first commit - just started

---
+++ FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by Thiago Heitling on 2016-01-21.
+//  Copyright © 2016 Thiago Heitling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
