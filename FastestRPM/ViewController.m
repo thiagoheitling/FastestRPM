4086fb72695a37c2abf8448b128307d9eac4fc73
Thiago Heitling <thiago.heitling@gmail.com>
2016-01-21 17:32:40 -0800

    assigment done

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -10,13 +10,42 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *needle;
+@property(nonatomic) CGPoint lastPoint;
+@property(nonatomic) CGPoint speed;
+@property(nonatomic) float speedMagnitude;
+@property(nonatomic) float minSpeedPosition;
+
 @end
 
 @implementation ViewController
 
+- (IBAction)speed:(UIPanGestureRecognizer *)sender {
+    
+    self.speed = [sender velocityInView:self.view];
+    self.speedMagnitude = sqrtf((self.speed.x * self.speed.x) + (self.speed.y * self.speed.y));
+    float speedRate = self.speedMagnitude / 4000;
+    
+    float degreesToRadians = (M_PI / 180);
+    self.minSpeedPosition = (140 * degreesToRadians);
+    
+    if ( [sender state] == UIGestureRecognizerStateChanged) {
+        
+        self.needle.transform = CGAffineTransformMakeRotation(self.minSpeedPosition + self.minSpeedPosition * speedRate);
+
+    }
+
+    if ( [sender state] == UIGestureRecognizerStateEnded) {
+
+        self.needle.transform = CGAffineTransformMakeRotation(self.minSpeedPosition);
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.needle.transform = CGAffineTransformMakeRotation(140 * M_PI / 180);
+    
 }
 
 - (void)didReceiveMemoryWarning {
